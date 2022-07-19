68b2dd0aa9b4c3bc72a2683a02881de467a0fe4a
Saurabh-314 <maxxxjob314@gmail.com>
2022-07-19 23:35:00 +0530

    some problem has been fix

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { Index } from './Pages/Index';
 
 export const AppRoutes = () => {
     return (
-
+        // <BrowserRouter>
+            <div>
+                <Routes>
+                    <Route path='/' element={<Index />} />
+                    <Route path='/home' element={<Home />} />
+                </Routes>
+            </div>
+        // </BrowserRouter>
     )
 }
